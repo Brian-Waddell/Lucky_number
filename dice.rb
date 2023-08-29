36314db3bbe31a88c09d041eba35bac84b1cd2a8
Brian Waddell <139478254+Brian-Waddell@users.noreply.github.com>
2023-08-29 16:52:46 +0000

    The Target

--- dice.rb
+++ dice.rb
@@ -14,30 +14,50 @@ get("/giraffe") do
 end 
 
 get("/dice/2/6") do 
-  first_die = rand(1..6) 
-  second_die = rand(1..6)
-  sum = first_die + second_die
 
-  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @attempts = []
+  
+   2.times do 
+
+    @first_die = rand(1..6) 
+ 
+    @attempts.push(@first_die)
+
+  end
+  #sum = first_die + second_die
+
+  #@outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  
 
   erb(:two_six)
 end 
 
 get("/dice/2/10") do 
-  first_die = rand(1..10)
-  second_die = rand(1..10)
-  sum = first_die + second_die
+ 
+  @array = []
+   2.times do 
+    die = rand(1..10)
+    @array.push(die)
+   end
 
-  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}. "
+  #first_die = rand(1..10)
+  #second_die = rand(1..10)
+  #sum = first_die + second_die
+
+  #@outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}. "
 
   erb(:two_ten)
+  
 end 
 
 get("/dice/1/20") do 
-  random_number_generator = rand(1..20)
-  sum = random_number_generator
+  @array = []
+  1.times do 
+ @random_number_generator = rand(1..20)
+ @array.push(@random_number_generator)
+  end 
 
-  @outcome = "You rolled a #{random_number_generator} for a total of #{sum}. "
+  #@outcome = "You rolled a #{@random_number_generator}. "
 
   erb(:one_20) 
 end 
@@ -45,15 +65,34 @@ end
 
 get("/dice/5/4") do 
 
-dice1 = rand(1..4)
-dice2 = rand(1..4)
-dice3 = rand(1..4)
-dice4 = rand(1..4)
-dice5 = rand(1..4)
 
-sum = dice1 + dice2 + dice3 + dice4 + dice5
+  @rolls = []
+  5.times do 
+    dice = rand(1..4)
+    @rolls.push(dice)
 
-@outcome = "You rolled a #{dice1}, a #{dice2}, a #{dice3}, a #{dice4}, and last one rolled was #{dice5} for a total of #{sum}."
+  end 
+#dice1 = rand(1..4)
+#dice2 = rand(1..4)
+#dice3 = rand(1..4)
+#dice4 = rand(1..4)
+#dice5 = rand(1..4)
+
+#sum = dice1 + dice2 + dice3 + dice4 + dice5
+
+#@outcome = "You rolled a #{dice1}, a #{dice2}, a #{dice3}, a #{dice4}, and last one rolled was #{dice5} for a total of #{sum}."
 
 erb(:four_five)
+end 
+
+get("/dice/100/6") do
+   @rolls = []
+
+  100.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end 
+
+  erb(:one_hundred_six) 
 end 
